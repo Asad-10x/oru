97ecfc39111ae215748ea0debf2c7f76aa0d1d56
Muhammad Amir Shahid <bulcpers0n0l@gmail.com>
2024-12-17 00:53:09 +0500

    VerifyToken middleware added

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,3 +1,5 @@
+const { verifyToken } = require("../src/user/userService");
+
 const roleMiddleware = (requiredRole) => {
     return (req, res, next) => {
         const { role } = req.user;
@@ -7,3 +9,22 @@ const roleMiddleware = (requiredRole) => {
         next();
     };
 };
+
+// Middleware to verify token
+const verifyTokenMiddleware = (req, res, next) => {
+    try {
+        const token = req.headers['authorization']?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ status: false, message: 'Authorization token is missing' });
+        }
+
+        const decoded = verifyToken(token);
+        req.user = decoded; // Attach decoded user data to request
+        next();
+    } catch (err) {
+        res.status(401).json({ status: false, message: 'Invalid or expired token' });
+    }
+};
+
+
+module.exports = { verifyTokenMiddleware, roleMiddleware };
