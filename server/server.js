e772c2711bcb624a6e33e87aceca915afba7b795
Muhammad Amir Shahid <bulcpers0n0l@gmail.com>
2024-12-17 00:59:40 +0500

    Database connection logic added as seperate module

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./route/userRoutes');
+const { connectDB } = require('./db/connection');
 
 // Initializing Express app
 const app = express();
+
+//connect database
+connectDB();
+
+// middlewares
 app.use(express.json());
 app.use(cors());
+
 app.use("/api/v1/users",userRoutes);
 
-// const databaseName = process.env.DATABASE || 'default_db';
-// Connect MongoDB
-// mongoose.connect(`${process.env.MONGO_URI}/${databaseName}`)
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log(`Connected to MongoDB Database`))
-.catch(err => console.log('Server MongoDB Connection Error: ', err));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost: ${PORT}`));
