4c8ebfefffa896291f98ced26218bc93754397ca
Muhammad Amir Shahid <bulcpers0n0l@gmail.com>
2024-12-19 02:57:59 +0500

    Role middleware added where applicable

--- server/route/taskRoutes.js
+++ server/route/taskRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('../src/task/taskController');
-const { verifyTokenMiddleware } = require('../middleware/authMiddleware');
+const { verifyTokenMiddleware, roleMiddleware } = require('../middleware/authMiddleware');
 
 // Create a new task or sub-task
-router.route('/create').post(verifyTokenMiddleware, taskController.createTask);
+router.route('/create').post(verifyTokenMiddleware, roleMiddleware('Lead'), taskController.createTask);
 
 // Fetch tasks (main tasks by default, or sub-tasks if parentTask is provided)
 router.route('/getAll').get(verifyTokenMiddleware, taskController.fetchTasks);
@@ -13,12 +13,12 @@ router.route('/getAll').get(verifyTokenMiddleware, taskController.fetchTasks);
 router.route('/:taskId').get(verifyTokenMiddleware, taskController.getTaskById);
 
 // Update task status
-router.route('/:taskId/status').patch(verifyTokenMiddleware, taskController.updateTaskStatus);
+router.route('/:taskId/status').patch(verifyTokenMiddleware, roleMiddleware('Lead'), taskController.updateTaskStatus);
 
 // Delete a task or sub-task
-router.route('/:taskId').delete(verifyTokenMiddleware, taskController.deleteTask);
+router.route('/:taskId').delete(verifyTokenMiddleware, roleMiddleware('Lead'), taskController.deleteTask);
 
 // Add a comment to a task
-router.route('/:taskId/comment').post(verifyTokenMiddleware, taskController.addCommentToTask);
+router.route('/:taskId/comment').post(verifyTokenMiddleware, roleMiddleware('Lead'), taskController.addCommentToTask);
 
 module.exports = router;
