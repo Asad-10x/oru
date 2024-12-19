affb956e3711b08ca7c1877bfec501ca01a043ce
Muhammad Amir Shahid <bulcpers0n0l@gmail.com>
2024-12-19 15:06:41 +0500

    Login API integrated

--- ui/src/app/components/pages/login/login.component.ts
+++ ui/src/app/components/pages/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component  } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {NavbarComponent} from '../../static/navbar/navbar.component';
+import { LoginService } from '../../../services/login/login.service';
 
 
 @Component({
@@ -10,20 +11,33 @@ import {NavbarComponent} from '../../static/navbar/navbar.component';
     imports: [NavbarComponent, ReactiveFormsModule, CommonModule, RouterLink],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
+    standalone:true
 })
 export class LoginComponent {
     userForm: FormGroup;
+    errorMessage: string = '';
 
-    constructor() {
+    constructor(private loginService:LoginService, private router: Router) {
         this.userForm = new FormGroup({
             email: new FormControl('',[Validators.required, Validators.email]),
             password: new FormControl('',[Validators.required, Validators.minLength(6)])
         })
     }
 
-    onSubmit(form: any){
-        if(form.valid){
-            console.log(this.userForm);
+    onSubmit(form:any) {
+        if (form.valid) {
+          const { email, password } = this.userForm.value;
+          this.loginService.login(email, password).subscribe(
+            (response) => {
+              console.log('Login successful:', response);
+              localStorage.setItem('token', response.token); // Store token in local storage
+              this.router.navigate(['/dashboard']); // Redirect to dashboard
+            },
+            (error) => {
+              console.error('Login failed:', error);
+              this.errorMessage = error.error.message || 'Login failed. Please try again.';
+            }
+          );
         }
     }
 
