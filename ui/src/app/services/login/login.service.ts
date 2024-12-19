9abef17ac84e985df62a912a1003c1721f710fcf
Muhammad Amir Shahid <bulcpers0n0l@gmail.com>
2024-12-19 10:42:47 +0500

    Login service itegration in progress

---
+++ ui/src/app/services/login/login.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, NgModule } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LoginService {
+    private loginUrl = 'http://localhost:5000/api/v1/users/login';
+
+
+    constructor(private http: HttpClient) {}
+
+    login(email: string, password: string): Observable<any> {
+        const headers = new HttpHeaders({
+          'Content-Type': 'application/json',
+        });
+        return this.http.post(this.loginUrl, { email, password }, { headers });
+      }
+
+}
